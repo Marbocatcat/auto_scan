784bbdcc7fde518e7c7af0061f80bd3ba855fc76
mar bocatcat <mar.bocatcat@gmail.com>
2019-05-25 14:25:14 -0500

    --quite on all commands

--- security_scan.sh
+++ security_scan.sh
@@ -9,33 +9,36 @@
 
 
 # check if clamav is install and install if needed
-rpm -qa | grep clamav || sudo dnf install clamav -y
-rpm -qa | grep rkhunter || sudo dnf install rkhunter -y
+rpm --quiet -qa | grep clamav || sudo dnf install clamav -y --quiet
+rpm --quiet -qa | grep rkhunter || sudo dnf install rkhunter -y --quiet
 
 # check for new definition then recursively scan /home directory for viruses
-clam_av():
+clam_av(){
 
-freshclam 
+sudo freshclam --quiet 
 if [ $? -eq 0 ]; then
-  clamscan $HOME
-  exit 0
+  # recursively check home folder and transfer logs when viruses are found
+  sudo clamscan -i -r --quiet $HOME --log=/var/log/clamav/viruses.log
 else
   exit 1
 fi
+}
 
 # check for new rootkit definition then check for rootkits
 
-rkhunter():
+rkhunter(){
 
-sudo rkhunter --update 
+sudo rkhunter --quiet --update 
 if [ $? -eq 0 ] || [ $? -eq 2 ]; then
-  sudo rkhunter --check --skip-keypress
-  exit 0
+  sudo rkhunter --quiet --check --skip-keypress
 else
   exit 1
 fi
+}
 
 clam_av
 rkhunter
+
+echo "clam_av & rkhunter scan finished!" | touch /home/bobcat/scan.notice
 
 
